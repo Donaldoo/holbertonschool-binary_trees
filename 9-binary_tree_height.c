77d6d86ce266e3be030824ce659e314af45bf3f7
Donaldoo <donald.kumaraku@gmail.com>
2022-08-31 06:52:57 -0700

    update

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,4 +1,7 @@
 #include "binary_trees.h"
+
+#define MAX(A, B) (((A) < (B)) ? (B) : (A))
+
 /**
  * binary_tree_height - returns height of tree
  * @tree: pointer to tree root node
@@ -12,8 +15,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	h_left = binary_tree_height(tree->left) + 1;
 	h_right = binary_tree_height(tree->right) + 1;
-	if (h_left > h_right)
-		return (h_left);
-	else
-		return (h_right);
+
+	return (MAX(h_left, h_right));
 }

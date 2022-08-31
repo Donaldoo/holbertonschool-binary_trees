2fb92964acd13432343fb44383d1f40d6f9d13c2
viviani22 <vivianxhumetiku@gmail.com>
2022-08-31 05:44:34 -0700

    task 6

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+#include <stdio.h>
+/**
+ * binary_tree_preorder - Goes through the tree in preorder and prints n
+ * @tree: pointer to head of tree
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL)
+		return;
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}

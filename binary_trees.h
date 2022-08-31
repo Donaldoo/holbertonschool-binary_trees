0946a356124e29c434aa8c27f7698ae786656ef4
viviani22 <vivianxhumetiku@gmail.com>
2022-08-31 04:43:14 -0700

    header update

--- binary_trees.h
+++ binary_trees.h
@@ -1,10 +1,7 @@
-#ifndef BINARY_TREES
-#define BINARY_TREES
-
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 #include <stddef.h>
-
+#include <stdlib.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -13,22 +10,14 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-};
-
-/* Types for binary trees */
-typedef struct binary_tree_s binary_tree_t;
-typedef struct binary_tree_s bst_t;
-typedef struct binary_tree_s avl_t;
-typedef struct binary_tree_s heap_t;
-
-
-/* Functions prototypes */
+} binary_tree_t;
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -48,8 +37,5 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
-size_t _height(const binary_tree_t *tree);
-void binary_tree_print(const binary_tree_t *tree);
-
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 #endif

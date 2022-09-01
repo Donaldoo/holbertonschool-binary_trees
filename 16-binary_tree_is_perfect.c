531a885118793757979c9202b917b1412b60fca7
viviani22 <vivianxhumetiku@gmail.com>
2022-09-01 02:43:47 -0700

    task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -11,10 +11,14 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if (binary_tree_balance(tree) == 0)
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	else
+	if (binary_tree_balance(tree) != 0)
 		return (0);
+	if (tree->left != NULL && tree->right != NULL)
+		return ((binary_tree_is_perfect(tree->left)) &&
+				binary_tree_is_perfect(tree->right));
+	return (0);
 }
 /**
  * binary_tree_balance - finds the balance of a binary tree
